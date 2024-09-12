3b3889d96895cfa203ad5a4c085bc0f84aff6118
ibtihelbs <ibtihel.bensalah@outlook.fr>
2024-09-12 11:55:23 +0200

    new stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Contact from "../componants/Contact";
+import { motion, useScroll, useTransform } from "framer-motion";
 import Hero from "../componants/Hero";
 import Products from "../componants/Products";
 import More from "../componants/More";
@@ -8,6 +9,9 @@ import { InfiniteSlider } from "../componants/InfiniteSlider";
 import H1 from "../componants/H1";
 
 const Home = () => {
+  const { scrollYProgress } = useScroll();
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, 360]); // Rotate from 0 to 360 degrees
+
   return (
     <div className="px-4 md:px-10">
       <Hero />
@@ -51,17 +55,18 @@ const Home = () => {
           <XlText content={"- A Guilty Pleasure in Every Bite"} />
         </InfiniteSlider>
 
-        <div className="grid grid-cols-5 gap-4 mt-6  m-auto">
-          <picture>
-            <source srcset="./no-bg/1_200x205.png" media="(max-width: 800px)" />
-            <source srcset="./no-bg/1_100x103.png" media="(max-width: 500px)" />
-            <img
+        <div className="grid grid-cols-5 gap-4 mt-6 m-auto">
+          <picture className="scale-[1.75] -translate-x-1/3">
+            <source srcSet="./no-bg/1_200x205.png" media="(max-width: 800px)" />
+            <source srcSet="./no-bg/1_100x103.png" media="(max-width: 500px)" />
+            <motion.img
               src="./no-bg/1.png"
               alt="croissant"
-              className="scale-[1.75] -translate-x-1/2"
+              style={{ rotate }}
             />
           </picture>
-          <div className="row-span-2 col-span-3  self-center p-4">
+
+          <div className="row-span-2 col-span-3 self-center p-4">
             <H1 content={"cakes so rich your bank account might get jealous"} />
             <Paragraph
               content={`We've got everything you need to
@@ -70,33 +75,34 @@ const Home = () => {
           we won’t do that because, well, life’s about risks, isn’t it?).`}
             />
           </div>
-          <picture>
-            <source srcset="./no-bg/2_200x203.png" media="(max-width: 800px)" />
-            <source srcset="./no-bg/2_100x102.png" media="(max-width: 500px)" />
-            <img
+
+          <picture className="scale-[1.75] translate-x-1/3">
+            <source srcSet="./no-bg/2_200x203.png" media="(max-width: 800px)" />
+            <source srcSet="./no-bg/2_100x102.png" media="(max-width: 500px)" />
+            <motion.img
               src="./no-bg/2.png"
               alt="croissant"
-              className="scale-[1.75] translate-x-1/2"
+              style={{ rotate }}
             />
           </picture>
-          <picture>
-            <source srcset="./no-bg/3_210x205.png" media="(max-width: 800px)" />
-            <source srcset="./no-bg/3_110x107.png" media="(max-width: 500px)" />
 
-            <img
+          <picture className="scale-[1.75] -translate-x-1/4 translate-y-1/2">
+            <source srcSet="./no-bg/3_210x205.png" media="(max-width: 800px)" />
+            <source srcSet="./no-bg/3_110x107.png" media="(max-width: 500px)" />
+            <motion.img
               src="./no-bg/3.png"
               alt="croissant"
-              className="scale-[1.75] translate-y-1/2"
+              style={{ rotate }}
             />
           </picture>
-          <picture>
-            <source srcset="./no-bg/4_250x205.png" media="(max-width: 800px)" />
-            <source srcset="./no-bg/4_122x100.png" media="(max-width: 500px)" />
 
-            <img
+          <picture className="scale-[1.75] translate-x-1/4 translate-y-1/2">
+            <source srcSet="./no-bg/4_250x205.png" media="(max-width: 800px)" />
+            <source srcSet="./no-bg/4_122x100.png" media="(max-width: 500px)" />
+            <motion.img
               src="./no-bg/4.png"
               alt="croissant"
-              className="scale-[1.75] translate-y-1/2"
+              style={{ rotate }}
             />
           </picture>
         </div>
