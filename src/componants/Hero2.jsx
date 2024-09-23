2a878975e4a89d22566bcd106b660042408995d8
ibtihelbs <ibtihel.bensalah@outlook.fr>
2024-09-23 14:21:17 +0200

    amazing <3

--- src/componants/Hero2.jsx
+++ src/componants/Hero2.jsx
@@ -9,13 +9,13 @@ const Hero2 = () => {
       className="md:mt-16 mt-4  w-[96%] m-auto  flex flex-col gap-4 "
     >
       <XxxlText content={"SWEET DREAMS"} style={"underline"} />
-      <div className="flex justify-between order-2 md:order-none ">
+      <div className="flex justify-between  items-start order-2 md:order-none ">
         <Paragraph
           style={"max-w-96 "}
           content={`You’re here because your cravings won, and your doctor hasn’t caught on yet. But hey, who’s counting pastries anyway?`}
         />
         <Link
-          className=" py-2 px-4 mt-4 inline-block md:mt-6 rounded-full border border-solid border-white hover:bg-white hover:text-black transition duration-300 ease-in-out"
+          className=" hover:bg-white hover:text-black transition duration-300 ease-in-out"
           to={"/shop"}
         >
           <div className=" relative cursor-pointer  h-[100px] w-[100px] border-black border border-solid rounded-[50%] ">
@@ -24,6 +24,7 @@ const Hero2 = () => {
               alt="arrow"
               className=" w-10 absolute  -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2"
             />
+
             <CircularSpinner
               width={100}
               textStyle={{ fontWeight: "bolder", fontFamily: "Yeseva One" }}
@@ -33,11 +34,20 @@ const Hero2 = () => {
           </div>
         </Link>
       </div>
-      <img
-        src="./hero.jpg"
-        alt="hero"
-        className="m-auto border border-solid border-black w-full rounded-lg"
-      />
+      <div
+        className="w-full h-[300px] border border-solid border-black  rounded-lg"
+        style={{
+          backgroundImage: "url('./mini.jpg')",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <img
+          src="./hero.jpg"
+          alt="hero"
+          className="m-auto w-full h-full object-cover "
+        />
+      </div>
     </section>
   );
 };
